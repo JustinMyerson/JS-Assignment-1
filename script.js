3aee6ab7ec2025f16967c024842d91bf1cef7e5e
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-03-15 14:39:24 +0200

    Add JS code

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const myConstant = 10;
+const myConstant = 10
+let myVariable = "Hello world"
+console.log(myVariable)
+
+const myArray = ["a", "b", "c", "d", "e"]
+
+myArray.forEach(element => {
+  console.log(element)
+});
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i])
+}
+
+const animal = {
+  type: "cat",
+  name: "Meep",
+}
+
+console.log(animal.type + " " + animal.name)
